3e50126162839a10bb9d6e3ebdb1a3b9ed0719f7
guruprashanth78 <guruprashanth78@gmail.com>
2023-10-31 22:44:45 +0530

    fix: fixed heading sizes and font for markdown renderer

--- components/Dashboard/RichText.vue
+++ components/Dashboard/RichText.vue
@@ -29,7 +29,7 @@ onMounted(() => {
 <template>
   <ClientOnly
     ><div
-      class="overflow-y-scroll hover:prose-a:underline prose-a:text-blue-800 prose-code:text-sm prose-code:--code-linear-bg prose-lg prose-blue h-full !pt-24 text-sm dark:prose-invert prose-headings:font-sans prose-code:font-inter prose-headings:text-4xl prose-headings:font-bold prose-headings:tracking-tight prose-pre:bg-zinc-300 prose-pre:text-gray-800 prose-ol:list-decimal prose-ul:list-disc sm:m-0 sm:p-5 px-3 sm:px-5 prose-p:text-[16px] prose-li:text-base prose-li:leading-4 prose-p:leading-6"
+      class="overflow-y-scroll hover:prose-a:underline prose-a:text-blue-800 prose-code:text-sm prose-code:--code-linear-bg prose-lg prose-blue h-full !pt-24 text-sm dark:prose-invert prose-headings:font-inter prose-code:font-inter prose-h1:text-4xl prose-headings:font-bold prose-headings:tracking-tight prose-pre:bg-zinc-300 prose-pre:text-gray-800 prose-ol:list-decimal prose-ul:list-disc sm:m-0 sm:p-5 px-3 sm:px-5 prose-p:text-[16px] prose-li:text-base prose-li:leading-4 prose-p:leading-6"
       v-html="$mdRenderer.render(block.content)"
     ></div
   ></ClientOnly>
